b445ae8f3bb827cef5acef327c0615759d9bf874
ev12 <notericv@gmail.com>
2020-05-03 17:00:20 -0700

    PD9

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%d9#is2_bjs+ldb2&npwi0l68-4h#bjj+czzdoj#)uqimbm+t('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
